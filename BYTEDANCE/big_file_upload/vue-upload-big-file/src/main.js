f9bd7216373767115cf4f949f5348908ff7aaf52
chenxinchu <cc1742692285@icloud.com>
2020-02-09 21:27:36 +0800

    大文件上传及断点续传

--- BYTEDANCE/big_file_upload/vue-upload-big-file/src/main.js
+++ BYTEDANCE/big_file_upload/vue-upload-big-file/src/main.js
@@ -1,8 +1,10 @@
 import Vue from 'vue'
 import App from './App.vue'
-import ElenentUI from 'element-ui';
+
+import ElementUI from "element-ui";
 import "element-ui/lib/theme-chalk/index.css";
-Vue.use(ElenentUI);
+Vue.use(ElementUI);
+
 Vue.config.productionTip = false
 
 new Vue({
