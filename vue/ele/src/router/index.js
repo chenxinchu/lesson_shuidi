1d1716a97982204cbfc7ec1125df734d03d2815a
chenxinchu <cc1742692285@icloud.com>
2020-02-28 20:05:47 +0800

    webpack

--- vue/ele/src/router/index.js
+++ vue/ele/src/router/index.js
@@ -1,6 +1,9 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
 import Home from '../views/Home.vue'
+// @代表什么? 
+// 延迟加载组件，当用户切换路由时，再来加载显示
+const GoodsList = resolve => require(['@/components/GoodsList'], resolve);
 
 Vue.use(VueRouter)
 
@@ -9,15 +12,20 @@ const routes = [
     path: '/',
     name: 'Home',
     component: Home
+  },
+  {
+    path: '/about',
+    name: 'About',
+    // route level code-splitting
+    // this generates a separate chunk (about.[hash].js) for this route
+    // which is lazy-loaded when the route is visited.
+    component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
+  },
+  {
+    path: '/goodsList', // 商品列表
+    name: 'GoodsList',
+    component: GoodsList
   }
-  // {
-  //   path: '/about',
-  //   name: 'About',
-  //   // route level code-splitting
-  //   // this generates a separate chunk (about.[hash].js) for this route
-  //   // which is lazy-loaded when the route is visited.
-  //   component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
-  // }
 ]
 
 const router = new VueRouter({
