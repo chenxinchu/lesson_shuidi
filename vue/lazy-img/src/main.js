c05e137eca0d20c49e0882eca617d0ae997bc404
chenxinchu <cc1742692285@icloud.com>
2020-03-11 21:08:51 +0800

    图片懒加载

---
+++ vue/lazy-img/src/main.js
@@ -0,0 +1,16 @@
+import Vue from 'vue'
+import App from './App.vue'
+
+import VueLazyload from 'vue-lazyload'
+
+Vue.use(VueLazyload,{
+  error:require('./assets/loading.svg'),
+  loading:require('./assets/loading.svg'),
+  attempt:1
+})
+
+Vue.config.productionTip = false
+
+new Vue({
+  render: h => h(App),
+}).$mount('#app')
