8b50e4a226ead8eb45d19d80a027a762e11f4b38
chenxinchu <cc1742692285@icloud.com>
2020-03-13 16:36:00 +0800

    golang

--- leetcode/leetcode 169/main.go
+++ leetcode/leetcode 169/main.go
@@ -1,3 +1,14 @@
-func majorityElement(nums []int) int {
-	
+func majorityElement(nums []int)  int{
+	majority,count := nums[0], 1
+	for i :=1; i < len(nums);	i++ {
+		if count == 0{
+			majority = nums[i]
+		}
+		if nums[i] == majority{
+			count++;
+		}else{
+			count--;
+		}
+	}
+	return majority;
 }
