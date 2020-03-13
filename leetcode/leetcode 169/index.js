f5d156f72bffa7370cb5c4bdc7177f96b2857e2d
chenxinchu <cc1742692285@icloud.com>
2020-03-13 09:21:30 +0800

    leetcode 169

---
+++ leetcode/leetcode 169/index.js
@@ -0,0 +1,17 @@
+var majorityElement = function(nums) {
+	var count=0
+	var maj = nums[0]
+	for(var i=0;i<nums.length;i++){
+		if(maj==nums[i]){
+			count++
+		}else{
+			count--;
+			if(count==0){
+				maj=nums[i+1]
+			}
+		}
+	}
+	return maj
+};
+console.log(majorityElement([2,3,3]));
+
