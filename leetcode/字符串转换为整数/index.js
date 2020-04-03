4e35c90ce202ace5d00575fbaee9a2bb45c39b63
chenxinchu <cc1742692285@icloud.com>
2020-04-03 20:14:55 +0800

    8

--- leetcode/字符串转换为整数/index.js
+++ leetcode/字符串转换为整数/index.js
@@ -1,14 +1,16 @@
-// var myAtoi = function (str) {
-//   if (parseFloat(str) <= Math.pow(-2, 31) || parseFloat(str) >= Math.pow(2, 31) - 1) { // 判断字符串溢出
-//     return Math.pow(-2, 31);
-//   }
-//   // if (parseInt(str)) return 0;
-//   // return parseInt(str);
-//   return Math.pow(-2, 31);
-// };
-var myAtoi = function(str) {
-  return Math.max(Math.min(parseInt(str) || 0, 2147483647), -2147483648)
+var myAtoi = function (str) {
+  if (parseInt(str) <= Math.pow(-2, 31)) { // 判断字符串溢出
+    return Math.pow(-2, 31);
+  }
+  if (parseInt(str) >= Math.pow(2, 31) - 1) {
+    return Math.pow(2, 31) - 1;
+  }
+  if (parseInt(str)==false) {
+    return 0
+  }
+  return parseInt(str);
 };
-console.log(myAtoi("4193 with words"));
-console.log(myAtoi("words and 987"));
-console.log(myAtoi("-91283472332"));
+// var myAtoi = function(str) {
+//   return Math.max(Math.min(parseInt(str) || 0, 2147483647), -2147483648)
+// };
+console.log(myAtoi("with words"));
