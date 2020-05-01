fc9901ae46f20e4bb4c7678f4e555d057047dfa4
chenxinchu <cc1742692285@icloud.com>
2020-05-01 16:52:38 +0800

    TS

---
+++ Typescript/tsaxios/src/type/index.ts
@@ -0,0 +1,9 @@
+export type Method = 'get' | 'GET'
+  | 'post' | 'POST' | 'put' | 'PUT'
+// & |:
+export interface AxiosRequestConfig {
+  url: string,
+  method?: Method,
+  data?: any,   // 
+  params?: any,
+}
