c59be693b5a029ecff179abc6a032cd7c9bc7fdc
chenxinchu <cc1742692285@icloud.com>
2020-05-22 22:50:19 +0800

    ipc

---
+++ electron/electron_control/main/ipc.js
@@ -0,0 +1,10 @@
+const { ipcMain, clipboard } = require('electron')
+
+module.exports = function () {
+  ipcMain.on('share-to-wechat', async (e, code) => {
+    if (code) {
+      // console.log(code)
+      clipboard.writeText(code.toString())
+    }
+  })
+}
